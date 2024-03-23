7c5083fa89d66a606d19105c6fa6bd4df295d51e
Alexander <104249649+suyxhs@users.noreply.github.com>
2024-03-23 11:24:02 +0300

    add categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import MainPage from '@/components/templates/MainPage/MainPage'
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Page Main</h1>
-    </main>
-  )
+  return <MainPage />
 }
